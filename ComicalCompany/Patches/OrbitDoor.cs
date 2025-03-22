a17d5e3275a98667e9d6e1af012f0fe998e69581
Temmie <76136819+Temmie3754@users.noreply.github.com>
2025-03-22 11:54:02 +0000

    Initial idea 3,11 implementation

---
+++ ComicalCompany/Patches/OrbitDoor.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace ComicalCompany.Patches
+{
+    public class OrbitDoor
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StartOfRound), "ShipHasLeft")]
+        public static void ShipHasLeft()
+        {
+            UnityEngine.Object.FindObjectOfType<HangarShipDoor>().SetDoorButtonsEnabled(true);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(HangarShipDoor), "SetDoorButtonsEnabled")]
+        public static void SetDoorButtonsEnabled(ref bool doorButtonsEnabled)
+        {
+            doorButtonsEnabled = true;
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HangarShipDoor), "SetDoorOpen")]
+        public static void SetDoorOpen()
+        {
+            if (StartOfRound.Instance.inShipPhase)
+            {
+                StartOfRound.Instance.FirePlayersAfterDeadlineClientRpc(new int[] {
+                    StartOfRound.Instance.gameStats.daysSpent,
+                    StartOfRound.Instance.gameStats.scrapValueCollected,
+                    StartOfRound.Instance.gameStats.deaths,
+                    StartOfRound.Instance.gameStats.allStepsTaken
+                }, true);
+            }
+        }
+        
+    }
+}
---
+++ ComicalCompany/Patches/PlayerControllerB.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.InputSystem;
+
+namespace ComicalCompany.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    public class PlayerControllerB
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerControllerB), "Jump_performed")]
+        public static void Jump_performed(InputAction.CallbackContext context, ref bool __runOriginal)
+        {
+            // 5% chance to not jump
+            if (UnityEngine.Random.Range(0, 100) < 2)
+            {
+                ComicalCompany.Logger.LogInfo("Cancelled jumping!");
+                __runOriginal = false;
+
+            }
+        }
+    }
+}
